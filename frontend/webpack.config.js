dcfbd80f4d4071d235a2f4a7d36f6408f2c1a305
nikita-skobov <nskobov.websites@gmail.com>
2018-11-26 17:59:24 -0600

    removes jest, adds some webpack plugins/loaders
    closes #1 closes #2

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,6 +1,25 @@
+const path = require('path')
+const HtmlWebPackPlugin = require('html-webpack-plugin')
+
 module.exports = {
   entry: ['./src/entry.js'],
   watch: false,
+  optimization: {
+    splitChunks: {
+      chunks: 'all',
+    },
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'scripts/bundle.[contenthash].js',
+    // chunkFileName: 'bundle.[contenthash].js',
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: './src/index.html',
+      filename: './index.html',
+    }),
+  ],
   module: {
     rules: [
       {
@@ -11,7 +30,19 @@ module.exports = {
       {
         test: /\.css$/,
         loader: 'style-loader!css-loader'
-      }
-    ]
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader",
+          },
+        ],
+      },
+    ],
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
   }
 };
